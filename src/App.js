9ba877733d7ab99984b68f1b87bbcd716bd3bb86
Rachel Rondon <rachel.sun.rondon@gmail.com>
2017-07-14 22:53:04 -0400

    add select-field

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './MyAwesomeReactComponent';
+import SelectFieldComponent from './SelectFieldComponent';
+import MenuItem from 'material-ui/MenuItem';
+
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Hello Rachel!</h2>
-        </div>
-        <p className="App-intro">
-        </p>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <MuiThemeProvider>
+    <div>
+      <MyAwesomeReactComponent />
+      <SelectFieldComponent />
+    </div>
+  </MuiThemeProvider>
+);
 
 export default App;
---
+++ src/SelectFieldComponent.js
@@ -0,0 +1,50 @@
+import React, {Component} from 'react';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+
+const names = [
+  'Rachel',
+  'Sarah',
+  'Rebecca'
+]
+
+const styles = {
+  customWidth: {
+    // width: 250,
+  },
+};
+
+
+export default class SelectFieldComponent extends Component{
+  state = {
+    values: [],
+  };
+
+  handleChange = (event, index, values) => this.setState({values});
+
+  menuItems(values) {
+    return names.map((name) => (
+      <MenuItem
+        key={name}
+        insetChildren={true}
+        checked={values && values.indexOf(name) > -1}
+        value={name}
+        primaryText={name}
+      />
+    ));
+  }
+
+  render() {
+    const {values} = this.state;
+    return(
+      <SelectField
+        multiple={true}
+        hintText="Select a name"
+        value={values}
+        onChange={this.handleChange}
+      >
+        {this.menuItems(values)}
+      </SelectField>
+    );
+  }
+}
